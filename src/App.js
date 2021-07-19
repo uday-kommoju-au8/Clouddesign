037774bdd2394c1c8d751f29d9437281cb87acde
uday-kommoju-au8 <udaynagasivakumar@gmail.com>
2021-07-19 20:42:04 +0530

    Assignment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Proptypes from 'prop-types';
+import Column from './containers/Column';
+import { DndProvider } from 'react-dnd';
+import {HTML5Backend} from 'react-dnd-html5-backend';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="container-fluid">
+          <div className="row taskContainer">
+            { this.props.columns.map((column, index) => (
+              <Column column={column} key={index} index={index} dispatch={this.props.dispatch} />
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
+App.propTypes = {
+  columns: Proptypes.array.isRequired,
+};
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    columns: state.columns,
+  };
+};
+export default DndProvider(HTML5Backend)(connect(mapStateToProps)(App));
